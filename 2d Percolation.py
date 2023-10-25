ec65e0ba9de8f37f4de8e101476c1f0c93526977
OliverLunn <136323103+OliverLunn@users.noreply.github.com>
2023-10-25 10:57:04 +0100

    Update 2d Percolation.py
    Updating lattice occupation. (removing sites with 0.5 occupation)

--- 2d Percolation.py
+++ 2d Percolation.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import label
 
 size = 100 
-p = 0.6 
+p = 0.6
+
 rand_lattice = np.random.rand(size,size) 
-occupied_lattice = rand_lattice<p 
-labeled_lattice,num = label(occupied_lattice)
+occupied_lattice = rand_lattice < p 
+labeled_lattice, num = label(occupied_lattice)
 count = np.bincount(np.reshape(labeled_lattice,size*size))
 count[0] = 0
 max_cluster_id = np.argmax(count)
-lattice = np.where(labeled_lattice == max_cluster_id,0.5,0)
-lattice = np.where(occupied_lattice == True, lattice+0.5,lattice)
-plt.imshow(lattice,cmap='gray')
+lattice = np.where(labeled_lattice == max_cluster_id,1,0)
+lattice = np.where(occupied_lattice == True, lattice,lattice)
+
+plt.imshow(lattice,cmap='binary')
 plt.show()
