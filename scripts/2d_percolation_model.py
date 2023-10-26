b91e408440ce2381c5f0936ffe59b98739612e2a
Harry-Neal <ppyhn1@nottingham.ac.uk>
2023-10-26 10:39:36 +0100

    resloving confilcts

--- scripts/2d_percolation_model.py
+++ scripts/2d_percolation_model.py
@@ -4,78 +4,76 @@ import matplotlib.pyplot as plt
 from random import uniform
 from scipy.ndimage import label
 
-def lattice_random(p, size):
-    
+class Percolation_2D():
     """
-    generates a 2d lattice of randomly distributed numbers 0 < x < 1.
-    A random number < probability is the condition for occupation of a site
-    Inputs:
-        p : occupation probaility [type : float]
-        size : lattice dimensions [type : int]
-            only ave capabiity for square lattice currently
-    Returns:
-        lattice : array of occupied/non-occupied sites
-    -------
+    Class contiaing functions for simulating 2 dimensional percolation transitions
     """
-    
-    rows, columns = size, size
-
-    lattice = [[0 for i in range(rows)] for j in range(columns)]
-    for i in range(rows):
-        for j in range(columns):
-            lattice[i][j] = uniform(0,1) <= p
-    
-    return lattice
-
-
-def cluster_search(lattice):
-    """
-    Searches lattice array of occupied sites for clusters.
-    
-    Inputs:
-        lattice : lattice of random numbers
+    def lattice_random(p, size):
         
-    Returns:
-        labeled_lattice : lattice with individual clusters labelled 
-
-    """
-    labeled_lattice, num = label(lattice)
-    return labeled_lattice    
-    
-def max_cluster(lattice, size):
-    """
-    Searches lattice array of occupied sites for largest cluster.
-    
-    Inputs:
-        lattice : lattice of occupied/non-occupied sites [2d array]
-        size : dimensions of lattice [float]
+        """
+        generates a 2d lattice of randomly distributed numbers 0 < x < 1.
+        A random number < probability is the condition for occupation of a site
+        Inputs:
+            p : occupation probaility [type : float]
+            size : lattice dimensions [type : int]
+                only ave capabiity for square lattice currently
+        Returns:
+            lattice : array of occupied/non-occupied sites
+        -------
+        """
         
-    Returns:
-        lmax_cluster : lattice with only max cluster [2d array] 
+        rows, columns = size, size
 
-    """
-    
-    count = np.bincount(np.reshape(lattice, size*size))
-    count[0] = 0
-    max_cluster_id = np.argmax(count)
-    max_cluster = np.where(lattice == max_cluster_id,1,0)
-    
-    return max_cluster
+        lattice = [[0 for i in range(rows)] for j in range(columns)]
+        for i in range(rows):
+            for j in range(columns):
+                lattice[i][j] = uniform(0,1) <= p
+        
+        return lattice
+
+
+    def cluster_search(lattice):
+        """
+        Searches lattice array of occupied sites for clusters.
+        
+        Inputs:
+            lattice : lattice of random numbers
+            
+        Returns:
+            labeled_lattice : lattice with individual clusters labelled 
+
+        """
+        labeled_lattice, num = label(lattice)
+        return labeled_lattice    
+        
+    def max_cluster(lattice, size):
+        """
+        Searches lattice array of occupied sites for largest cluster.
+        
+        Inputs:
+            lattice : lattice of occupied/non-occupied sites [2d array]
+            size : dimensions of lattice [float]
+            
+        Returns:
+            lmax_cluster : lattice with only max cluster [2d array] 
+
+        """
+        
+        count = np.bincount(np.reshape(lattice, size*size))
+        count[0] = 0
+        max_cluster_id = np.argmax(count)
+        max_cluster = np.where(lattice == max_cluster_id,1,0)
+        
+        return max_cluster
 
 
 if __name__ == '__main__':
     p = 0.59274621  #transition prob
     size = 1000
     
-<<<<<<< Updated upstream
-    lattice_1 = lattice_random(p, size)             #generate lattice
-    labeled_lattice_1 = cluster_search(lattice_1)           #label lattice
-    max_cluster_1 = max_cluster(labeled_lattice_1, size)    #find max cluster
-=======
-    lattice_1 = Percolation_2D.lattice_rand(p, size)             #generate lattice
+    lattice_1 = Percolation_2D.lattice_random(p, size)             #generate lattice
     labeled_lattice_1 = Percolation_2D.cluster_search(lattice_1)           #label lattice
     max_cluster_1 = Percolation_2D.max_cluster(labeled_lattice_1, size)    #find max cluster
->>>>>>> Stashed changes
     
     fig, (ax1,ax2) = plt.subplots(1,2, sharey=True)    #plot 
     ax1.imshow(labeled_lattice_1, cmap="viridis")
