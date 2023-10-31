55a1984398227c96b0994a1c45c3fe45c40cc3ee
unknown <ppyol1@nottingham.ac.uk>
2023-10-31 10:14:10 +0000

    commit

--- scripts/1d_percolation.py
+++ scripts/1d_percolation.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     gamma_array = []
-    probs = np.arange(0.1,1,0.0001)
+    probs = np.arange(0.0001,1,0.0001)
+    p_c = 1
 
     def gamma(probs, L, gamma_array):
         for p in probs:
@@ -13,16 +14,20 @@ if __name__ == '__main__':
            gamma_array = np.append(gamma_array, gamma)
 
         return gamma_array
+    
+    def cluster_size(probs):
+        cluster_size = (p_c+probs)/(p_c-probs)
+        return cluster_size
 
-    ax1 = plt.axes()
+    fig, (ax1) = plt.subplots(1,1)
     ax1.plot(probs, gamma(probs, 1, gamma_array), label="L=1")
     ax1.plot(probs, gamma(probs, 2, gamma_array), label="L=2")
     ax1.plot(probs, gamma(probs, 3, gamma_array), label="L=3")
     ax1.plot(probs, gamma(probs, 5, gamma_array), label="L=5")
-    ax1.plot(probs, gamma(probs, 20, gamma_array), label="L=20")
-    ax1.plot(probs, gamma(probs, 50, gamma_array), label="L=50")
+    ax1.plot(probs, gamma(probs, 25, gamma_array), label="L=20")
     ax1.plot(probs, gamma(probs, 100, gamma_array), label="L=100")
     ax1.legend()
     ax1.set_ylabel("$\Pi(p,L)$")
     ax1.set_xlabel("Occupation probabilty, p")
-    plt.show()
+
+    plt.show() 
