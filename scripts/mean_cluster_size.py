e12823a089ba089cdd1064c45b3ee9f572dbfdb1
unknown <ppyol1@nottingham.ac.uk>
2023-11-03 10:26:40 +0000

    avg_cluster size

--- scripts/mean_cluster_size.py
+++ scripts/mean_cluster_size.py
@@ -9,31 +9,43 @@ import scipy.optimize as opt
 
 from main import Percolation2D
 
+def avg_size_plot(array, size, probs, reps):
+
+    for r in tqdm(range(0,reps)):
+        i=0 
+        for p in probs:
+            lattice = percolation2d.lattice_random(size, p)
+            labeled_lattice = percolation2d.cluster_search(lattice)
+            array[i,r] = percolation2d.average_cluster_size(labeled_lattice)
+            i += 1
+        
+    return np.average(array,axis=1)
+
 if __name__ == '__main__':
 
     percolation2d = Percolation2D() #create class object
 
     p1 = 0.59274605079210  #transition prob
     size = 200
-    probs = np.arange(0.1,0.9,0.005)
-    rep = 3
-    avg_size = np.zeros((len(probs), rep))
+    probs = np.arange(0.1,0.99,0.01)
+    reps = 5
 
-    for r in tqdm(range(0,rep)):
-        i=0
-        for p in probs:
-            lattice = percolation2d.lattice_random(size, p)
-            labeled_lattice = percolation2d.cluster_search(lattice)
-            avg_size[i,r] = percolation2d.average_cluster_size(labeled_lattice)
+    avg_size_50 = np.zeros((len(probs), reps))
+    avg_size_100 = np.zeros((len(probs), reps))
+    avg_size_150 = np.zeros((len(probs), reps))
+    avg_size_200 = np.zeros((len(probs), reps))
 
-            i += 1
-    ydata = np.average(avg_size,axis=1)
     fig,(ax1) = plt.subplots(1,1)
 
-    ax1.plot(probs, ydata, "ko", label="lattice")
-    ax1.vlines(p1,0,1, linestyles='--', color='black', label="$p_c$")
-    ax1.set_ylabel("Average Cluster Size, $\zeta_p$", fontsize="20")
+    ax1.plot(probs, avg_size_plot(avg_size_50, 150, probs, reps), "ro",label="L:150")
+    ax1.plot(probs, avg_size_plot(avg_size_50, 175, probs, reps), "b<",label="L:175")
+    ax1.plot(probs, avg_size_plot(avg_size_50, 200, probs, reps), "kD",label="L:200")
+    
+    ax1.vlines(p1,0,size**2, linestyles='--', color='black', label="$p_c$")
+    ax1.set_ylabel("$\\frac{M_2(p,L)}{M_1(p,L)}$", fontsize="20")
     ax1.set_xlabel("Probability, $p$", fontsize="20")
     ax1.tick_params(axis="x", labelsize=18)
     ax1.tick_params(axis="y", labelsize=18)
+
+    plt.legend()
     plt.show()
