4e0e3820ed462b67889a467a40c27e7011798014
Harry-Neal <ppyhn1@nottingham.ac.uk>
2023-11-08 13:51:53 +0000

    finalising

--- scripts/average_size.py
+++ scripts/average_size.py
@@ -9,43 +9,41 @@ def func(x,m,c):
     return m*x + c
 
 percolation2D = Percolation2D()
-sizes = [75,100,150,200,250,300]
-probs=np.linspace(0.4,0.7,75)
-avg_size = np.zeros((len(sizes),len(probs)))
+sizes = [50,75,100,250,200,250]
+probs=np.linspace(0.3,0.7,100)
+S = np.zeros((len(sizes),len(probs)))
 j=0
 for size in tqdm(sizes):
-    runs=int(1000*25/size)
+    runs=int(500*25/size)
     for r in tqdm(range(0,runs)):
         i=0
         lattice = percolation2D.rand_lattice(size)
         for p in probs:
             lattice_occupied= percolation2D.occupied(lattice,size,p)
-            avg_size[j,i] =avg_size[j,i] + percolation2D.average_cluster_size(lattice_occupied)
+            S[j,i] =S[j,i] + percolation2D.average_cluster_size(lattice_occupied)
             i+=1
-    avg_size[j,:] = avg_size[j,:]/((size**2)*runs)
+    S[j,:] = S[j,:]/((size**2)*runs)
     j+=1
 
 fig,(ax1)=plt.subplots(1)
-
+gammas = np.zeros(len(S))
+errs = np.zeros(len(S))
 p_c = 0.59274605079210
-gammas = np.zeros(len(sizes))
-errs = np.zeros(len(sizes))
-colors=['red','orange','yellow','green','blue','purple']
 indx = np.max(np.where(probs<p_c))
+colors=['red','orange','yellow','green','blue','purple']
 for j in range(len(sizes)):
-    ax1.plot(probs,avg_size[j,:],color=colors[j],label=f'L={int(sizes[j])}')
+    
+    ax1.plot(probs,S[j,:],color = colors[j],label=f'L={int(sizes[j])}')
 
-    #ax2.plot(np.log(np.abs(probs[0:indx]-p_c)),np.log(avg_size[j,0:indx]))
-    #np.savetxt(f'{size}x{size} lattice average size data over {runs} runs.txt',avg_size)
-
-    ppot,pcov = opt.curve_fit(func,np.log(np.abs(probs[0:indx]-p_c)),np.log(avg_size[j,0:indx]))
-    #ax2.plot(np.log(np.abs(probs[0:indx]-p_c)),func(np.log(np.abs(probs[0:indx]-p_c)),*ppot),color='black',label='fit')
-    gammas[j] = ppot[0]
+    ppot,pcov = opt.curve_fit(func,np.log(np.abs(probs[0:indx]-p_c)),np.log(S[j,0:indx]))
     errs[j] = np.sqrt(np.diag(pcov))[0]
-
-print(gammas,errs)
-print(np.average(gammas),np.average(errs))
-ax1.set_xlabel('$p$')
-ax1.set_ylabel('$S(p,L)$')
-ax1.legend()
+    gammas[j] = ppot[0]
+np.savetxt(f'data\square lattice average size data.txt',S)
+np.savetxt('data\gamma estimation square.txt',np.vstack((sizes,gammas,errs)))
+ax1.vlines(p_c,np.min(S[j,:]),np.max(S[j,:]),color='black',linestyle='--',label='P_c')
+ax1.legend(fontsize="18")
+ax1.tick_params(axis="x", labelsize=18)
+ax1.tick_params(axis="y", labelsize=18)
+ax1.set_xlabel('$P$',fontsize="22")
+ax1.set_ylabel('$S(p,L)$',fontsize="22")
 plt.show()
